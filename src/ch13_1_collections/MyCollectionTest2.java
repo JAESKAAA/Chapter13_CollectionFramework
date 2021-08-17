baad8989d0ffcac390451d20d3a655e371368784
JAESKAAA <78363202+JAESKAAA@users.noreply.github.com>
2021-08-18 00:13:59 +0900

    All issues were corrected !! XD

--- src/ch13_1_collections/MyCollectionTest2.java
+++ src/ch13_1_collections/MyCollectionTest2.java
@@ -11,9 +11,9 @@ public class MyCollectionTest2 {
 		 * 1.add 기능 구현(완료)
 		 * 2.get 기능 구현 (완료)
 		 * 3.size 기능 구현 (완료)
-		 * 4.ArrayIndexOutOfBoundsException 해결_자동으로 배열길이 늘려주는 기능
-		 * 5.remove 기능 구현
-		 * 6.배열 초기화_일괄삭제기능
+		 * 4.ArrayIndexOutOfBoundsException 해결_자동으로 배열길이 늘려주는 기능 (완료)
+		 * 5.remove 기능 구현 (완료)
+		 * 6.배열 초기화_일괄삭제기능 (완료)
 		 * 
 		 */
 		
@@ -22,18 +22,20 @@ public class MyCollectionTest2 {
 		list.add(3);
 		list.add(5);
 		//ArrayIndexOutOfBoundsException
-		//list.add(7);
+		list.add(7);
 		
 		
 		// 삭제 예시
 		//[1,3,5,] -> [1,5,] 즉, 사이즈도 줄어야하고 배열인덱스의 값도 없어져야 함
-//		list.remove(1);
-		
+		list.remove(1);
+	
 		//일괄 삭제 -> [0,0,0] 으로 나와야함
 		//list.clear(); 하면 배열이 초기상태로 만들어 주는 기능
 		System.out.println(list);
-		System.out.println("list[2]의 값: "+list.get(2));
+		System.out.println("list[1]의 값: "+list.get(1));
 		System.out.println("size :"+ list.size());
+		list.removeAll();
+		System.out.println(list);
 		System.out.println("==================================");
 	}
 
--- src/com/libs/collection/MyIntList.java
+++ src/com/libs/collection/MyIntList.java
@@ -14,9 +14,21 @@ public class MyIntList {
 	}
 	
 	//메소드
-	
 	public void add(int num) {
-			arr[cursor++] = num; //커서의 위치에 입력하는 값 저장
+		try {
+		arr[cursor++] = num; //커서의 위치에 입력하는 값 저장
+		}catch(ArrayIndexOutOfBoundsException e) { //예외 발생되면 하기와 같이 배열 새로 생성
+			int[] arr2 = new int[arr.length+1];
+			for(int i=0, k=0; i<=arr.length; i++) {
+				if(i==arr.length) {//배열의 길이가 i값과 같아진다 (배열이 꽉찼다)
+					arr2[k] = num; //arr2배열[k]에 num을 넣어주고 반복문 탈출
+					break;
+				}
+				arr2[k++] = arr[i]; // arr배열의 값을 arr2에 추가
+			}
+			arr=arr2; //arr2의 주소를 arr에 저장
+			
+		}
 	}
 
 	public int get(int idx) {
@@ -29,7 +41,22 @@ public class MyIntList {
 	}
 	
 	public void remove(int idx) {
-		
+		//먼저 배열값을 복사해서 새로운 배열 생성
+		int [] arr2 = new int[arr.length-1];
+		for(int i=0, k=0; i<arr.length; i++) {
+			if( i== idx) { //i값이 인덱스 번호와 같은 배열에는 배열 추가하지 않고 다시 조건문으로 올라감
+				continue;
+			}
+			arr2[k++] = arr[i];
+		}
+		arr=arr2;
+		cursor--; //배열을 삭제해줬으니 커서도 하나 줄여줘야함
+	}
+	
+	public void removeAll() {
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=0;
+		}
 	}
 	
 	
